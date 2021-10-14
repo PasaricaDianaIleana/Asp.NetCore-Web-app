6b8890e1be433b53439b23542f599ca0e17818f2
Pasarica Diana <pasaricadiana@yahoo.com>
2021-10-15 00:36:44 +0300

    Form validation

--- OnlineStore/Controllers/User/UserController.cs
+++ OnlineStore/Controllers/User/UserController.cs
@@ -8,11 +8,12 @@ namespace OnlineStore.Controllers.User
 {
     public class UserController : Controller
     {
-
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
