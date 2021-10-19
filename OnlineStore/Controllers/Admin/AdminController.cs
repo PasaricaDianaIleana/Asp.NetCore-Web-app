f1c24946550c8a4fa5f43d35d4235ac2c78676df
Pasarica Diana <pasaricadiana@yahoo.com>
2021-10-19 16:52:01 +0300

    Admin partial view methods

--- OnlineStore/Controllers/Admin/AdminController.cs
+++ OnlineStore/Controllers/Admin/AdminController.cs
@@ -3,11 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace OnlineStore.Controllers
 {
-    [Authorize]
+    
     public class AdminController : Controller
     {
         [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddPromotion()
         {
             return View();
         }
